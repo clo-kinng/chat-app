5873b2f3b62138120af2036b6e005d58e0a63650
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-06 12:08:30 +0900

    各モデルのテスト

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Message, type: :model do
+  describe '#create' do
+    before do
+      @message = FactoryBot.build(:message)
+    end
+
+    it 'contentとimageが存在すれば保存できる' do
+      expect(@message).to be_valid
+    end
+    it 'contentがからでも保存できる' do
+      @message.content = ''
+      expect(@message).to be_valid
+    end
+    it 'imageが空でも保存できる' do
+      @message.image = nil
+      expect(@message).to be_valid
+
+    end
+    it 'contentとimageが空では保存できないこと' do
+      @message.content = ''
+      @message.image = nil
+      @message.valid?
+      expect(@message.errors.full_messages).to include("Content can't be blank")
+    end
+    it 'roomが紐付いていなければ保存できないこと' do
+      @message.room = nil
+      @message.valid? 
+      expect(@message.errors.full_messages).to include("Room must exist")
+    end
+    it 'userが紐付いていなければ保存できないこと' do
+      @message.user = nil
+      @message.valid? 
+      expect(@message.errors.full_messages).to include("User must exist")
+    end
+  end
+end
